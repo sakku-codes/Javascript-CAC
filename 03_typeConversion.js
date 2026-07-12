50125833cd8a2606c41a63eb229ca3558721e234
Saksham Garg <gargsaksham137@gmail.com>
2026-07-12 13:27:37 +0530

    Learned about the type conversion iof various datatypes in javascript

---
+++ 03_typeConversion.js
@@ -0,0 +1,13 @@
+let Score = "33";
+let IntegerScore = Number(Score);
+
+console.log(IntegerScore);
+console.log(typeof(IntegerScore));
+console.log(typeof(Score));
+
+// if the entered value in the score is "33abc" then the output of the IntegerScore is NaN - not a number
+
+let isLoggedIn = 1;
+let boolLoggedIn = Boolean(isLoggedIn);
+
+console.log(boolLoggedIn);
