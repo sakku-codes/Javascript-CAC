e162f4af7e40d02194cc83d8e70da87023b9a67d
Saksham Garg <gargsaksham137@gmail.com>
2026-07-12 13:48:52 +0530

    added new concept of the Operators of js

---
+++ 04_Operations.js
@@ -0,0 +1,39 @@
+let str1 = "Saksham";
+let str2 = "Garg";
+
+// String concatenation 
+
+let str3 = str1 + str2 ;
+console.log(str3);
+
+
+console.log(1 + 3);
+console.log("1" + 3);
+console.log(1 + 2 + "3");
+console.log(1 + "2" + "3");
+console.log("1"+ "2" + 3);
+
+//observe the various outputs and understand the concepts
+
+// Comparison Operators : 
+
+console.log(2 > 1);
+console.log(2 != 2);
+
+console.log(null == 0);  // 0 is some value but the null represents nothing
+console.log(null > 0);   // null is not greater than the 0
+console.log(null >= 0);  // observe here it gives true because the  COMPARISON operator change or covert the type of the null to zero hence it give the value true 
+
+
+// Strict Check ===
+
+// the strict check don't allow the type coversion so it always used where the types of the two opearnds are same
+
+console.log("2" === 2);
+
+console.log(2 === 2);
+
+
+
+
+
