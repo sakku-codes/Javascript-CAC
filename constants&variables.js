ab29d24c53b2cabf60511dc5f2091734c09a8a8e
Saksham Garg <gargsaksham137@gmail.com>
2026-07-12 12:05:31 +0530

    learned about cont,var and let keywords

---
+++ constants&variables.js
@@ -0,0 +1,13 @@
+//const keyword for constants 
+
+const marks = 49;
+
+console.log(marks);
+
+// var keyword is used previously but due its global scope problem a new keyword is came into existence let that only has the block scope 
+
+var greet = "Hello to the viewers";
+
+let name = "Saksham Garg";
+console.log(greet);
+console.log(name);
